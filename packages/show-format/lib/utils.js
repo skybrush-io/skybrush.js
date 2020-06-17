bf8485162c024ed9819855181281c26bf7be72da
Tamas Nepusz <ntamas@gmail.com>
2020-06-17 11:14:17 +0200

    switch to setImmediate shim from core-js-pure

--- packages/show-format/lib/utils.js
+++ packages/show-format/lib/utils.js
@@ -1,17 +1,11 @@
-/* global requestIdleCallback */
+const setImmediate = require('core-js-pure/features/set-immediate');
 
 /**
- * Async function that blocks until the next idle cycle of the browser. Used to
- * ensure that the browser UI does not block when resolving references in the
- * show file.
- *
- * Resolves immediately in Node.js as there is no equivalent to requestIdleCallback()
- * there.
+ * Async function that blocks until the next idle cycle of the browser or the
+ * Node environment. Used to ensure that the browser UI does not block when
+ * resolving references in the show file.
  */
-const idle =
-  typeof requestIdleCallback !== 'undefined' /* istanbul ignore next */
-    ? async () => new Promise((resolve) => requestIdleCallback(resolve))
-    : () => {};
+const idle = () => new Promise((resolve) => setImmediate(resolve));
 
 module.exports = {
   idle
