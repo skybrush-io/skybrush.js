eb5819aaacd5f571ac5107a6524f1eb42d4e2984
Tamas Nepusz <ntamas@gmail.com>
2021-04-01 18:37:34 +0200

    feat: added BackgroundHunt and FormHeader to mui-components

--- packages/mui-components/src/index.js
+++ packages/mui-components/src/index.js
@@ -1,13 +1,19 @@
+import BackgroundHint from './BackgroundHint';
 import DialogAppBar from './DialogAppBar';
 import DialogTabs from './DialogTabs';
+import FormHeader from './FormHeader';
 import GenericHeaderButton from './GenericHeaderButton';
+import SemanticAvatar from './SemanticAvatar';
 import ThemeSelector from './ThemeSelector';
 import Tooltip from './Tooltip';
 
 export {
+  BackgroundHint,
   DialogAppBar,
   DialogTabs,
+  FormHeader,
   GenericHeaderButton,
+  SemanticAvatar,
   ThemeSelector,
   Tooltip,
 };
