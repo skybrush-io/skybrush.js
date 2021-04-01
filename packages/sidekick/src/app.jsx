7064067227b56db0e944f0053175a7bf1ab2b6ea
Tamas Nepusz <ntamas@gmail.com>
2021-04-01 10:51:39 +0200

    feat: added basic keymap

--- packages/sidekick/src/app.jsx
+++ packages/sidekick/src/app.jsx
@@ -1,15 +1,18 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import React from 'react';
+import { HotKeys } from 'react-hotkeys';
 import { Provider as StoreProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 
 import DialogsContainer from './dialogs';
+import keyMap from './keymap';
 import { persistor, store } from './store';
 import ThemeProvider from './theme';
 import TopLevelView from './views/TopLevelView';
 
 import 'typeface-fira-sans';
 
+import '../assets/css/screen.less';
 import '../assets/css/split-pane.less';
 import '../assets/css/tooltips.less';
 
@@ -18,7 +21,9 @@ const App = () => (
     <ThemeProvider>
       <PersistGate persistor={persistor}>
         <CssBaseline />
-        <TopLevelView />
+        <HotKeys root keyMap={keyMap}>
+          <TopLevelView />
+        </HotKeys>
         <DialogsContainer />
       </PersistGate>
     </ThemeProvider>
