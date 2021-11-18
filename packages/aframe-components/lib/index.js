c04dc7f713d4c600b5018f226cfde853c0401926
Tamas Nepusz <ntamas@gmail.com>
2021-11-18 23:06:11 +0100

    feat: moved spatial utilities from Skybrush Viewer to @skybrush/aframe-components

--- packages/aframe-components/lib/index.js
+++ packages/aframe-components/lib/index.js
@@ -1,6 +1,7 @@
 import AFrame from './_aframe';
+import * as spatial from './spatial';
 import { objectToString } from './utils';
 
-export { AFrame, objectToString };
+export { AFrame, objectToString, spatial };
 
 export default AFrame;
