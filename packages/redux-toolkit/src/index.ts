08c22735f6bbbe52113cc82773d89a3bcc136025
Tamas Nepusz <ntamas@gmail.com>
2022-02-13 22:53:53 +0100

    fix: fix TypeScript type re-exporting

--- packages/redux-toolkit/src/index.ts
+++ packages/redux-toolkit/src/index.ts
@@ -1,15 +1,24 @@
-import { configureStoreAndPersistence } from './store';
+import { StorageConfig as StorageConfig_ } from './persistence';
+import {
+  configureStoreAndPersistence,
+  StoreAndPersistenceConfig as StoreAndPersistenceConfig_,
+} from './store';
 
 export {
   chooseUniqueId,
   chooseUniqueName,
   chooseUniqueIdFromName,
 } from './naming';
-export { createStorageConfiguration, StorageConfig } from './persistence';
-export {
-  configureStoreAndPersistence,
-  StoreAndPersistenceConfig,
-} from './store';
+export { createStorageConfiguration } from './persistence';
+export { configureStoreAndPersistence } from './store';
 export { isAllowedInRedux, noPayload, stripEvent } from './utils';
+
+export type StorageConfig<S, RS = any, HSS = any, ESS = any> = StorageConfig_<
+  S,
+  RS,
+  HSS,
+  ESS
+>;
+export type StoreAndPersistenceConfig = StoreAndPersistenceConfig_;
 
 export default configureStoreAndPersistence;
