694e9327cbf0d3d8290d594eb6155567a6c3172a
Tamas Nepusz <ntamas@gmail.com>
2022-12-30 16:26:15 +0100

    feat: added AsyncGuard component, untested

---
+++ packages/mui-components/src/AsyncGuard.tsx
@@ -0,0 +1,65 @@
+import Error from '@mui/icons-material/Error';
+import Button from '@mui/material/Button';
+
+import React from 'react';
+import { useAsync } from '@react-hookz/web';
+
+import BackgroundHint from './BackgroundHint';
+import LargeProgressIndicator from './LargeProgressIndicator';
+
+export interface AsyncGuardProps<T> {
+  children: (value: T) => React.ReactNode;
+  errorMessage?: string;
+  func: () => Promise<T>;
+  loadingMessage?: string;
+  style?: React.CSSProperties;
+}
+
+function AsyncGuard<T>({
+  children,
+  func,
+  errorMessage,
+  loadingMessage,
+  style,
+}: AsyncGuardProps<T>) {
+  // style prop is forwarded to make this component play nicely when it is used
+  // as a top-level component in a transition
+
+  const [state, actions, _] = useAsync(func);
+
+  switch (state.status) {
+    case 'error':
+      return (
+        <BackgroundHint
+          icon={<Error />}
+          text={errorMessage ?? 'An unexpected error happened'}
+          button={
+            <Button
+              onClick={() => {
+                actions.reset();
+              }}
+            >
+              Try again
+            </Button>
+          }
+          style={style}
+        />
+      );
+
+    case 'loading':
+    case 'not-executed':
+      return (
+        <LargeProgressIndicator
+          fullHeight
+          label={loadingMessage ?? 'Please wait, loading...'}
+          style={style}
+        />
+      );
+
+    case 'success':
+      return children ? children(state.result!) : null;
+
+    default:
+      return null;
+  }
+}
