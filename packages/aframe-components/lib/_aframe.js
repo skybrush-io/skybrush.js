8d47f9efc10a8b273d3b411cb66291d14168fc0e
Tamas Nepusz <ntamas@gmail.com>
2024-08-26 16:23:27 +0200

    fix: remove AFrame import hackery

--- packages/aframe-components/lib/_aframe.js
+++ packages/aframe-components/lib/_aframe.js
@@ -2,9 +2,6 @@
  * Requiring all modules that we need in order to make AFrame work.
  */
 
-// Do not use require('aframe') -- it would load the compiled AFrame bundle,
-// which prevents us from using our custom fork for layout-bmfont-text that
-// prevents an issue with Content-Security-Policy
-const AFrame = require('aframe/src/index');
+const AFrame = require('aframe');
 
 export default AFrame;
