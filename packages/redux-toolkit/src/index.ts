68e5b128679a59a499aac5f81d0e28ab13b9d5d0
Tamas Nepusz <ntamas@gmail.com>
2025-12-22 15:14:05 +0100

    fix: also export SelectionHandlerThunk

--- packages/redux-toolkit/src/index.ts
+++ packages/redux-toolkit/src/index.ts
@@ -10,7 +10,10 @@ export {
 } from './naming.js';
 export { createStorageConfiguration } from './persistence.js';
 export { configureStoreAndPersistence } from './store.js';
-export type { SelectionHandlerReduxFunctions } from './types.js';
+export type {
+  SelectionHandlerReduxFunctions,
+  SelectionHandlerThunk,
+} from './types.js';
 export { isAllowedInRedux, noPayload, stripEvent } from './utils.js';
 
 export type StorageConfig<S, RS = any, HSS = any, ESS = any> = StorageConfig_<
