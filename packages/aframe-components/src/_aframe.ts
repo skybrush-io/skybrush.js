a4e3a6f688347046852d1ca0c4db60172bc99975
Tamas Nepusz <ntamas@gmail.com>
2025-12-22 19:36:02 +0100

    fix: patching @types/aframe because we know that it has a default export in AFrame 1.7

--- packages/aframe-components/src/_aframe.ts
+++ packages/aframe-components/src/_aframe.ts
@@ -2,7 +2,10 @@
  * Requiring all modules that we need in order to make AFrame work.
  */
 
-import * as AFrame from 'aframe';
+// Yes, this is correct. AFrame has a default export, at least in version 1.7 and
+// later. The typings provided by @types/aframe are incorrect (probably refer to an
+// older AFrame version) so we patch them.
+import AFrame from 'aframe';
 
 export const { THREE } = AFrame;
 
