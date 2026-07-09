0f7cdd475079cee2752d2d9aac56675f19b92930
Tamas Nepusz <ntamas@gmail.com>
2026-07-10 00:27:20 +0200

    feat: added inset prop to MiniListDivider

--- packages/mui-components/src/MiniListDivider.tsx
+++ packages/mui-components/src/MiniListDivider.tsx
@@ -1,11 +1,15 @@
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 
+type Props = {
+  inset?: number;
+};
+
 /**
  * Divider that fits nicely in a mini list.
  */
-const MiniListDivider = () => (
-  <Box sx={{ mx: -1, my: 1 }}>
+const MiniListDivider = ({ inset = 0 }: Props) => (
+  <Box sx={{ mx: inset - 1, my: 1 }}>
     <Divider />
   </Box>
 );
